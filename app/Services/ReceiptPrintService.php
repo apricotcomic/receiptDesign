0b8a1de888bc91def105f24d993b9223a823f93c
apricotcomic <apricotcomic@gmail.com>
2020-07-01 22:40:57 +0900

    PDFデータをbladeで作成

--- app/Services/ReceiptPrintService.php
+++ app/Services/ReceiptPrintService.php
@@ -15,26 +15,8 @@ class ReceiptPrintService
     {
         $this->pdf->AddPage();
         $this->pdf->SetFont("kozgopromedium", "", 10);
-        $html = <<< EOF
-            <style>
-            h1 {
-                font-size: 24px; // 文字の大きさ
-                color: #ff00ff; // 文字の色
-                text-align: center; // テキストを真ん中に寄せる
-            }
-            p {
-                font-size: 12px; // 文字の大きさ
-                color: #000000; // 文字の色
-                text-align: left; // テキストを左に寄せる
-            }
-            </style>
-            <h1>侍エンジニア塾</h1>
-            <p>
-            今日は侍エンジニア塾についてお話させていただきます。
-            </p>
-            EOF;
 
-        $this->pdf->writeHTML($html); // 表示htmlを設定
+        $this->pdf->writeHTML(view("form.sampleReceipt"));
         $this->pdf->Output('samurai.pdf', 'I');
     }
 }
---
+++ resources/views/form/sampleReceipt.blade.php
@@ -0,0 +1,53 @@
+<style>
+h1 {
+    font-size: 24px; // 文字の大きさ
+    color: #000000; // 文字の色
+    text-align: center; // テキストを真ん中に寄せる
+}
+div{
+    font-size: 12px;
+    color: #000000;
+    text-align: right;
+}
+p {
+    font-size: 12px; // 文字の大きさ
+    color: #000000; // 文字の色
+    text-align: center; // テキストを左に寄せる
+}
+</style>
+<h1>領収書</h1>
+<div>
+    〒111-2222　東京都新宿区1-1-1<br>
+    株式会社AAAA
+</div>
+<p>
+金 999,999,999 円<br>
+上記正に領収しました
+</p>
+<table>
+    <tr>
+        <th>商品</th>
+        <th>数量</th>
+        <th>単価</th>
+        <th>金額</th>
+    </tr>
+    <tr>
+        <td>商品１</td>
+        <td>1</td>
+        <td>100</td>
+        <td>100</td>
+    </tr>
+    <tr>
+        <td>商品２</td>
+        <td>4</td>
+        <td>200</td>
+        <td>800</td>
+    </tr>
+    <tr>
+        <td>商品３</td>
+        <td>3</td>
+        <td>150</td>
+        <td>450</td>
+    </tr>
+</table>
+
